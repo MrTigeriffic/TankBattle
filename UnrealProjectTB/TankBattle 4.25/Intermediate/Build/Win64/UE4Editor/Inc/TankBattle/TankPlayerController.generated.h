e2f7ac9eeb0f4f36b5dd16d5ebfb9e7f5268f90d
Oran Chadwick <oranchad@gmail.com>
2020-05-30 17:15:01 +0100

    Upload Part 3

---
+++ UnrealProjectTB/TankBattle 4.25/Intermediate/Build/Win64/UE4Editor/Inc/TankBattle/TankPlayerController.generated.h
@@ -0,0 +1,95 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TANKBATTLE_TankPlayerController_generated_h
+#error "TankPlayerController.generated.h already included, missing '#pragma once' in TankPlayerController.h"
+#endif
+#define TANKBATTLE_TankPlayerController_generated_h
+
+#define TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_15_SPARSE_DATA
+#define TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_15_RPC_WRAPPERS
+#define TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATankPlayerController(); \
+	friend struct Z_Construct_UClass_ATankPlayerController_Statics; \
+public: \
+	DECLARE_CLASS(ATankPlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/TankBattle"), NO_API) \
+	DECLARE_SERIALIZER(ATankPlayerController)
+
+
+#define TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesATankPlayerController(); \
+	friend struct Z_Construct_UClass_ATankPlayerController_Statics; \
+public: \
+	DECLARE_CLASS(ATankPlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/TankBattle"), NO_API) \
+	DECLARE_SERIALIZER(ATankPlayerController)
+
+
+#define TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATankPlayerController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATankPlayerController) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATankPlayerController); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATankPlayerController); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATankPlayerController(ATankPlayerController&&); \
+	NO_API ATankPlayerController(const ATankPlayerController&); \
+public:
+
+
+#define TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATankPlayerController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATankPlayerController(ATankPlayerController&&); \
+	NO_API ATankPlayerController(const ATankPlayerController&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATankPlayerController); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATankPlayerController); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATankPlayerController)
+
+
+#define TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_15_PRIVATE_PROPERTY_OFFSET
+#define TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_12_PROLOG
+#define TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_15_PRIVATE_PROPERTY_OFFSET \
+	TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_15_SPARSE_DATA \
+	TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_15_RPC_WRAPPERS \
+	TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_15_INCLASS \
+	TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_15_PRIVATE_PROPERTY_OFFSET \
+	TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_15_SPARSE_DATA \
+	TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_15_INCLASS_NO_PURE_DECLS \
+	TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> TANKBATTLE_API UClass* StaticClass<class ATankPlayerController>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID TankBattle_4_25_Source_TankBattle_Public_TankPlayerController_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
